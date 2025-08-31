24379926bb335ee1c2cc168a3c304d25b8461015
OSteam13 <oskarfunky13@gmail.com>
2025-08-31 23:45:10 +0600

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // важное место — не проверяем цепочку сертификатов у pooler'а
   ssl: { require: true, rejectUnauthorized: false },
 });
 
